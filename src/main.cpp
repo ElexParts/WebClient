323455eba9e39308497807e7ec6e0191dfe23331
Romualdo Dasig <dasig.rg@a5project.com>
2021-08-13 20:07:15 +0800

    Add a PubSubClient for Adafruit CC3000 WiFi Shield

--- src/main.cpp
+++ src/main.cpp
@@ -31,19 +31,23 @@ It might not work on all networks!
 #include <SPI.h>
 #include <string.h>
 #include "utility/debug.h"
+#include "cc3000_PubSubClient.h"
 
 // These are the interrupt and control pins
 #define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+
 // These can be any two pins
 #define ADAFRUIT_CC3000_VBAT  5
 #define ADAFRUIT_CC3000_CS    10
+
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
                                          SPI_CLOCK_DIVIDER); // you can change this clock speed
 
-#define WLAN_SSID       "myNetwork"           // cannot be longer than 32 characters!
-#define WLAN_PASS       "myPassword"
+#define WLAN_SSID   "myNetwork"      // Cannot be longer than 32 characters!
+#define WLAN_PASS   "myPassword"
+
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
@@ -51,10 +55,26 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
                                    // received before closing the connection.  If you know the server
                                    // you're accessing is quick to respond, you can reduce this value.
 
-// What page to grab!
-#define WEBSITE      "wifitest.adafruit.com"
-#define WEBPAGE      "/testwifi/index.html"
+// Configure a CC3000 Web Client.
+Adafruit_CC3000_Client client;
 
+// Configure an MQTT server.
+union ArrayToIp {
+  byte array[4];
+  uint32_t ip;
+};
+
+// Set MQTT server's ip address.
+ArrayToIp server = { 0, 0, 0, 0 };
+
+// MQTT Client callback.
+void callback (char* topic, byte* payload, unsigned int length) {
+  // I have not tested this at all
+  // I've gotten the publish up and running
+  // use at own risk
+}
+
+cc3000_PubSubClient mqttclient(server.ip, 1883, callback, client, cc3000);
 
 /**************************************************************************/
 /*!
@@ -67,12 +87,76 @@ uint32_t ip;
 
 /**************************************************************************/
 /*!
+    @brief  Displays the driver mode (tiny of normal), and the buffer
+            size if tiny mode is not being used
+
+    @note   The buffer size and driver mode are defined in cc3000_common.h
+*/
+/**************************************************************************/
+void displayDriverMode(void) {
+  #ifdef CC3000_TINY_DRIVER
+    Serial.println(F("CC3000 is configure in 'Tiny' mode"));
+  #else
+    Serial.print(F("RX Buffer : "));
+    Serial.print(CC3000_RX_BUFFER_SIZE);
+    Serial.println(F(" bytes"));
+    Serial.print(F("TX Buffer : "));
+    Serial.print(CC3000_TX_BUFFER_SIZE);
+    Serial.println(F(" bytes"));
+  #endif
+}
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the CC3000's internal firmware patch ID
+*/
+/**************************************************************************/
+uint16_t checkFirmwareVersion(void) {
+  uint8_t major, minor;
+  uint16_t version;
+  
+  #ifndef CC3000_TINY_DRIVER  
+    if(!cc3000.getFirmwareVersion(&major, &minor))
+    {
+      Serial.println(F("Unable to retrieve the firmware version!\r\n"));
+      version = 0;
+    }
+    else
+    {
+      Serial.print(F("Firmware V. : "));
+      Serial.print(major); Serial.print(F(".")); Serial.println(minor);
+      version = major; version <<= 8; version |= minor;
+    }
+  #endif
+
+  return version;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the 6-byte MAC address of the CC3000 module
+*/
+/**************************************************************************/
+void displayMACAddress(void) {
+  uint8_t macAddress[6];
+  
+  if(!cc3000.getMacAddress(macAddress))
+  {
+    Serial.println(F("Unable to retrieve MAC Address!\r\n"));
+  }
+  else
+  {
+    Serial.print(F("MAC Address : "));
+    cc3000.printHex((byte*)&macAddress, 6);
+  }
+}
+
+/**************************************************************************/
+/*!
     @brief  Begins an SSID scan and prints out all the visible networks
 */
 /**************************************************************************/
-
-void listSSIDResults(void)
-{
+void listSSIDResults(void) {
   uint32_t index;
   uint8_t valid, rssi, sec;
   char ssidname[33]; 
@@ -108,8 +192,7 @@ void listSSIDResults(void)
     @brief  Tries to read the IP address and other connection details
 */
 /**************************************************************************/
-bool displayConnectionDetails(void)
-{
+bool displayConnectionDetails(void) {
   uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
   
   if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
@@ -129,24 +212,37 @@ bool displayConnectionDetails(void)
   }
 }
 
-void setup(void)
-{
+void setup(void) {
   Serial.begin(115200);
   Serial.println(F("Hello, CC3000!\n")); 
-
   Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+  displayDriverMode();
   
-  /* Initialise the module */
+  // Initialise the module.
   Serial.println(F("\nInitializing..."));
-  if (!cc3000.begin())
-  {
-    Serial.println(F("Couldn't begin()! Check your wiring?"));
+  if (!cc3000.begin()) {
+    Serial.println(F("Unable to initialise the CC3000! Check your wiring?"));
+    while(1);
+  }
+
+  uint16_t firmware = checkFirmwareVersion();
+  if ((firmware != 0x113) && (firmware != 0x118)) {
+    Serial.println(F("Wrong firmware version!"));
+    for(;;);
+  }
+  
+  displayMACAddress();
+  
+  Serial.println(F("\nDeleting old connection profiles..."));
+  if (!cc3000.deleteProfiles()) {
+    Serial.println(F("Failed!"));
     while(1);
   }
   
   // Optional SSID scan
   // listSSIDResults();
   
+  // Connect to WiFi router.
   Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
   if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
     Serial.println(F("Failed!"));
@@ -155,75 +251,45 @@ void setup(void)
    
   Serial.println(F("Connected!"));
   
-  /* Wait for DHCP to complete */
-  Serial.println(F("Request DHCP"));
-  while (!cc3000.checkDHCP())
-  {
+  // Wait for DHCP to complete.
+  while (!cc3000.checkDHCP()) {
     delay(100); // ToDo: Insert a DHCP timeout!
   }  
 
-  /* Display the IP address DNS, Gateway, etc. */  
+  // Display the IP address DNS, Gateway, etc.
   while (! displayConnectionDetails()) {
     delay(1000);
   }
 
-  ip = 0;
-  // Try looking up the website's IP address
-  Serial.print(WEBSITE); Serial.print(F(" -> "));
-  while (ip == 0) {
-    if (! cc3000.getHostByName(WEBSITE, &ip)) {
-      Serial.println(F("Couldn't resolve!"));
-    }
-    delay(500);
+  // Connect to MQTT server.
+  Serial.println(F("\nConnecting to MQTT server..."));
+  if (!client.connected()) {
+    client = cc3000.connectTCP(server.ip, 1883);
   }
 
-  cc3000.printIPdotsRev(ip);
-  
-  // Optional: Do a ping test on the website
-  /*
-  Serial.print(F("\n\rPinging ")); cc3000.printIPdotsRev(ip); Serial.print("...");  
-  replies = cc3000.ping(ip, 5);
-  Serial.print(replies); Serial.println(F(" replies"));
-  */  
-
-  /* Try connecting to the website.
-     Note: HTTP/1.1 protocol is used to keep the server from closing the connection before all data is read.
-  */
-  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
-  if (www.connected()) {
-    www.fastrprint(F("GET "));
-    www.fastrprint(WEBPAGE);
-    www.fastrprint(F(" HTTP/1.1\r\n"));
-    www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n"));
-    www.fastrprint(F("\r\n"));
-    www.println();
-  } else {
-    Serial.println(F("Connection failed"));    
-    return;
-  }
-
-  Serial.println(F("-------------------------------------"));
-  
-  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
-  unsigned long lastRead = millis();
-  while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
-    while (www.available()) {
-      char c = www.read();
-      Serial.print(c);
-      lastRead = millis();
+  // Publish successful connection status to MQTT server.
+  if(client.connected()) {
+    Serial.println(F("Connected to MQTT."));
+    if (mqttclient.connect("ArduinoUnoClient-CC3000-A4")) {
+      mqttclient.publish("sensors/a4/out/debug","A4 is now online");
     }
   }
-  www.close();
-  Serial.println(F("-------------------------------------"));
-  
-  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
-  /* the next time your try to connect ... */
-  Serial.println(F("\n\nDisconnecting"));
-  cc3000.disconnect();
-  
 }
 
-void loop(void)
-{
- delay(1000);
+void loop(void) {
+  if (!client.connected()) {
+    // Try connecting to MQTT server.
+    client = cc3000.connectTCP(server.ip, 1883);
+  
+    if(client.connected()) {
+      if (mqttclient.connect("ArduinoUnoClient-Office-A4")) {
+        mqttclient.publish("sensors/a4/out/debug","A4 is now back online");
+      }
+    }
+  } else {
+    // Publish to MQTT server.
+    mqttclient.publish("sensors/a4/out", "28");
+  }
+
+  delay(5000);
 }
